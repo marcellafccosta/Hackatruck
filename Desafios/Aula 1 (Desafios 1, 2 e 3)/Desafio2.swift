071f4d5500b4ce5b4c770e381cd2bd871f64d2e5
Marcella Chaves <127541337+marcellafccosta@users.noreply.github.com>
2025-03-25 17:12:24 -0300

    Add files via upload

---
+++ Desafios/Aula 1 (Desafios 1, 2 e 3)/Desafio2.swift
@@ -0,0 +1,31 @@
+//
+//  Desafio2.swift
+//  Aula1
+//
+//  Created by Turma01-18 on 19/03/25.
+//
+
+import SwiftUI
+
+struct Desafio2: View {
+    var body: some View {
+        HStack(spacing: 50){
+            Image("truck")
+                .resizable()
+                .frame(width: 120, height: 120)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+
+            VStack{
+                Text("HackaTruck")
+                    .foregroundStyle(.red)
+                Text("77 Universidades")
+                    .foregroundStyle(.blue)
+                Text("5 regiões do Brasil")
+                    .foregroundStyle(.yellow)
+            }
+        }}
+}
+
+#Preview {
+    Desafio2()
+}
